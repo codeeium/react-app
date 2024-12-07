4433dafa605aed27ccb58a161228c2ce2f0221c3
codeeium <codeeium@gmail.com>
2024-12-07 15:24:40 +0000

    v7 check box updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,18 @@
-import React from 'react';
-import Toggle from './Toggle';
+import React, { useState } from 'react';
+import Checkbox from './Checkbox';
 
 const App: React.FC = () => {
+    const [checked, setChecked] = useState<boolean>(false);
+
+    const handleCheckboxChange = (isChecked: boolean) => {
+        setChecked(isChecked);
+    };
+
     return (
         <div>
-            <h1>React Toggle Button with TypeScript</h1>
-            <Toggle />
+            <h1>React TypeScript Checkbox Example</h1>
+            <Checkbox checked={checked} onChange={handleCheckboxChange} />
+            <p>Checkbox is {checked ? 'checked' : 'unchecked'}</p>
         </div>
     );
 };
