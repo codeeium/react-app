369e8f17e01289ae19f220ed65de20f996c6b240
codeeium <codeeium@gmail.com>
2024-12-08 16:26:01 +0000

    todo empty state

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
-import './index.css'
 import 'bootstrap/dist/css/bootstrap.css'
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
@@ -9,3 +8,4 @@ ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <App />
   </React.StrictMode>,
 )
+q
