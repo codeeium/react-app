fa5918e7fdf8a9522a94ca2723138fa88527aa1a
codeeium <codeeium@gmail.com>
2024-12-10 22:58:30 +0000

    app.tsx file updated

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import React from 'react';
 const App: React.FC = () => {
     return (
         <div>
-            <h1>Hello, World!</h1>
+            <h1 className='align-content-sm-center'>Hello, World!</h1>
         </div>
     );
 };
