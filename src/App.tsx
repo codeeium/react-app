01aea2bf2ce9b427dc44a9563f99f9384fb7448b
codeeium <codeeium@gmail.com>
2024-12-11 17:37:06 +0000

    app file updated

--- src/App.tsx
+++ src/App.tsx
@@ -4,15 +4,6 @@ import { AppData } from './data';    // Importing AppData
 const App: React.FC = () => {
     return (
         <>
-            {/*{AppData.map(*/}
-            {/*    (props, index) => (*/}
-            {/*    <div key={index}>*/}
-            {/*        <h1>Name: {props.name}</h1>*/}
-            {/*        <h2>Age: {props.age}</h2>*/}
-            {/*        <h2>Gender: {props.gender}</h2>*/}
-            {/*    </div>*/}
-            {/*))}*/}
-
             {AppData.map(
                 (props, index) => (
                 <div key={index}>
