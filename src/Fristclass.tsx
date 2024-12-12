51fa84ec427a299a17491b59d3741c348d39a0c5
codeeium <codeeium@gmail.com>
2024-12-12 16:46:10 +0000

    First class tsx updated

---
+++ src/Fristclass.tsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+const Fristclass = () => {
+    return (
+        <>
+        </>
+    )
+}
+
+/*
+
+✅Day 1:  Introduction to React | Node
+
+Installation | Setting up React environment
+
+✅Day 2 : Introduction to JSX| Components | Basic React component creation. | Styling
+
+in React
+
+✅Day 3: Life Cycle Method | Learn about functional components.
+
+✅Day 4 : Learn Class Based Components
+
+✅Day 5 : Props | Passing props to components.
+
+ */
