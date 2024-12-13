d64aee8860000dd6b16b5d6cbe4c761747a7d383
codeeium <codeeium@gmail.com>
2024-12-13 12:41:59 +0000

    useReducer in js

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
-import Counter from './counter';
+import Counter from './counter.jsx';
 
-const App: React.FC = () => {
+const App = () => {
     return (
         <>
             <Counter />
