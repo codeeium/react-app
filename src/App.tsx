c4978a582cfb1337d724e3313b08447f049e1847
codeeium <codeeium@gmail.com>
2024-12-13 02:22:17 +0000

    hello world debugging updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 
 const App: React.FC = () => {
+    const message = 'Hello, World!';
     return (
         <div>
-            <h1 className='align-content-sm-center'>Hello, World!</h1>
+            <h1 className='align-content-sm-center'>{message}</h1>
+            <h1 className='align-content-sm-center'>{message}</h1>
         </div>
     );
 };
