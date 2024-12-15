d7703bdbb4248052c873cea9919995ac3dec8cb8
codeeium <codeeium@gmail.com>
2024-12-15 19:55:51 +0000

    ts-main file updated

--- src/ts-main.tsx
+++ src/ts-main.tsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import TSAppWithFormat from './TS-App-with-format';
 import TSAppWithFormat from './TS-App-with-format';
+// import AppWithFormat from './ts-App'
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
 root.render(
