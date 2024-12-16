e8839d74f9c7d9581d82c296ec1a3c98b4e01c80
codeeium <codeeium@gmail.com>
2024-12-16 18:57:56 +0000

    idea file are removed

--- src/TSX/App.tsx
+++ src/TSX/App.tsx
@@ -5,7 +5,8 @@ const App: React.FC = () => {
 
     useEffect(() => {
         const entries = performance.getEntriesByType("navigation");
-        const navigation = entries.length > 0 ? entries[0] as PerformanceNavigationTiming : null;
+        const navigation =
+            entries.length > 0 ? entries[0] as PerformanceNavigationTiming : null;
 
         if (navigation) {
             console.log("Navigation type:", navigation.type); // Log the navigation type
