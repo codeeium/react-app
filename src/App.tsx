892f5a2d847eb393c6cae4d1be5c95c8bbb51d6b
codeeium <codeeium@gmail.com>
2024-12-16 17:40:55 +0000

    use effect updated

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,24 @@
-// import Message from "./Message";
+import React, { useEffect } from "react";
 import ListGroup from "./ListGroup";
 
 function App() {
+    useEffect(() => {
+        // Get navigation entries from the performance API
+        const navigation = performance.getEntriesByType("navigation")[0];
+
+        // Type assertion to let TypeScript know it's a PerformanceNavigationTiming object
+        if (navigation && (navigation as PerformanceNavigationTiming).type === "reload") {
+            console.log("Page was reloaded");
+        } else {
+            console.log("Page loaded normally");
+        }
+
+        // Optional cleanup if needed
+        return () => {
+            console.log("Component is unmounting");
+        };
+    }, []);
+
     return (
         <div className="App">
             <ListGroup />
