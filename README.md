f494f98f11c33be4c2d0a15f489168332b884efd
codeeium <codeeium@gmail.com>
2024-12-19 20:26:00 +0000

    Action(Reducer)->Stor -> UI components data flow

--- README.md
+++ README.md
@@ -1,4 +1,9 @@
 
+![img_4.png](img_4.png)
+
+
+
+
 ![img_3.png](img_3.png)
 
 
---
+++ src/JSX-Increment/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import UICounter from './components/UI-Counter.jsx';
+
+const App = () => {
+    return (
+        <div>
+            <UICounter />
+        </div>
+    );
+};
+
+export default App;
