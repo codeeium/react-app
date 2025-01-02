52fdf94d9eee74447d713012c732a9aeef611f6a
codeeium <codeeium@gmail.com>
2025-01-02 20:55:01 +0000

    java learn

--- per-project/src/main.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import UserProfile from "./PropDrilling/useState_propDrilling.jsx";
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <UserProfile w/>
-  </React.StrictMode>
-)
--- per-project/src/performance/LazyComponent.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function LazyComponent() {
-    return (
-        <div>
-            <h2>This is a Lazy Loaded Component!</h2>
-        </div>
-    );
-}
