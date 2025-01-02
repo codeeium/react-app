02d666f40acab23fedf46621fb186e4857c0cbc2
codeeium <codeeium@gmail.com>
2025-01-02 23:56:29 +0000

    updated

---
+++ Javalearn/src/main/java/org/example/AppVariable.java
@@ -0,0 +1,26 @@
+package org.example;
+
+public class AppVariable {
+
+    public static void main(String[] args) {
+
+        String firtName = "sam";
+        String lastName = "Naidu";
+
+        int myNum = 5;
+        float myFloatNum = 5.99f;
+        char myLetter = 'D';
+        boolean myBool = true;
+        String myText = "Hello";
+
+        System.out.println(firtName+' ' + lastName);
+        System.out.println(myNum+' ' + myFloatNum);
+        System.out.println(myLetter +' ' + myText);
+        System.out.println(myBool );
+    }
+}
+
+
+
+
+
