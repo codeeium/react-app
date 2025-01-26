88e5b71a7d995747d20489a359c497e029bff52b
codeeium <codeeium@gmail.com>
2025-01-26 23:14:50 +0000

    ip address index.js file updated 0.0.0.0

--- api/backend/index.js
+++ api/backend/index.js
@@ -160,7 +160,7 @@ app.get('/api/profile', async (req, res) => {
 // Start the Server
 const PORT = process.env.PORT || 5038;
 connectToDatabase().then(() => {
-    app.listen(5038, "cosmic-kids.zapto.org", () => {
+    app.listen(5038, '0.0.0.0', () => {
         console.log('Server running on port 5038');
     });
 });
