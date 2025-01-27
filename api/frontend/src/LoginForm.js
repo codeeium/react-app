c7177911975d56ed5aa01b594205565d613598d5
codeeium <codeeium@gmail.com>
2025-01-27 02:43:31 +0000

    .env updated

--- api/frontend/src/LoginForm.js
+++ api/frontend/src/LoginForm.js
@@ -11,7 +11,7 @@ const LoginForm = ({ onLoginSuccess }) => {
 
         try {
             // const response = await fetch('http://localhost:5038/api/login', {
-            const response = await fetch('http://localhost:5038/api/login', {
+            const response = await fetch('http://13.40.188.131:5038/api/login', {
                 method: 'POST',
                 headers: { 'Content-Type': 'application/json' },
                 body: JSON.stringify({ username, password }),
